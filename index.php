cbc6a5c7ce15dd0b7567d4218111de5f91c0316a
James Byrne <jamesdesbyrne@gmail.com>
2016-06-28 17:15:55 +0100

    Adding the addtocart api for adding products to the users cart

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 use Omnipay\Common\CreditCard;
 use Omnipay\Omnipay;
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 
 require __DIR__.'/vendor/autoload.php';
 
@@ -24,38 +27,78 @@ $app->before(function () use ($app) {
   $app['twig']->addGlobal('baseurl', $app['request']->getBaseUrl());
 });
 
+// Setup application to recieve requests as JSON
+$app->before(function (Request $request) {
+  if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+    $data = json_decode($request->getContent(), true);
+    $request->request->replace(is_array($data) ? $data : array());
+  }
+});
+
 // root route
 $app->get('/', function () use ($app) {
-  $products = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  // Open the CSV file
+  $file = fopen(__DIR__.'/assets/products/products.csv', 'r');
+
+  // Load the CSV file
+  $i = 0;
+  while(! feof($file)) {
+    $products[$i] = fgetcsv($file);
+    $i++;
+  }
+
+  // Close the CSV file
+  fclose($file);
+
+  // Render the template
   return $app['twig']->render('index.twig', array(
     'products' => $products
   ));
 });
 
 $app->get('/cart', function () use ($app) {
-  // Cart page
+  // Render the template
+  return $app['twig']->render('cart.twig', array(
+    'session' => $app['session']->get('cart')
+  ))
 });
 
-$app->get('/checkout', function () use ($app) {
-  // Cart page
-  // $gateway = Omnipay::create('Realex_Remote');
-  //
-  // $formData = array('number' => '4242424242424242', 'expiryMonth' => '6', 'expiryYear' => '2016', 'cvv' => '123', 'transactionId' => '1');
-  //
-  // $response = $gateway->purchase(array('amount' => '10.00', 'currency' => 'USD', 'card' => $formData, 'transactionId' => '1'))->send();
-  //
-  // if ($response->isSuccessful()) {
-  //   // payment was successful: update database
-  //   print_r($response);
-  // } elseif ($response->isRedirect()) {
-  //   // redirect to offsite payment gateway
-  //   $response->redirect();
-  // } else {
-  //   // payment failed: display message to customer
-  //   echo $response->getMessage();
-  // }
 
+// Add a product to the cart
+$app->post('/api/addtocart', function (Request $request) use ($app) {
+
+  // Create an empty php object
+  $product = new stdClass();
+
+  // Assign properties to the product
+  $product->name = $request->request->get('name');
+  $product->price = $request->request->get('price');
+
+  if(null !== $app['session']->get('cart')) {
+    // Get the current array
+    $cart = $app['session']->get('cart');
+
+    // Append the new products into the array
+    array_push($cart, $product);
+
+    // Save the new cart into the session
+    $app['session']->set('cart', $cart);
+
+    // Return the new cart
+    return $app->json($cart, 201);
+  } else {
+    // Set the cart to the current products
+    $app['session']->set('cart', array($product));
+
+    // Get the new carts values
+    $cart = $app['session']->get('cart');
+
+    // Return the new cart
+    return $app->json($cart, 202);
+  }
 });
+
+$app->get('/checkout', function () use ($app) {});
 
 $app->run();
 
