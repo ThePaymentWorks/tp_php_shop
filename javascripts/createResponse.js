2eadbd93c0c1be7be1c3dc478f5a948c9fdbd133
James Byrne <jamesdesbyrne@gmail.com>
2016-07-01 16:46:01 +0100

    Styling and creation of the responses (success/error)
    If the 00 (success) code is returned we will apply the success styling,
    for everything else the error styling is applied. These are defined
    within the css and are customizable via the themes sectionIf the 00
    (success) code is returned we will apply the success styling, for
    everything else the error styling is applied. These are defined within
    the css and are customizable via the themes section.

--- javascripts/createResponse.js
+++ javascripts/createResponse.js
@@ -1,21 +1,46 @@
 function createResponse (rawXML) {
-
-  const xmldoc = $.parseXML(res);
+  const xmldoc = $.parseXML(rawXML);
   const $xml = $(xmldoc);
   const result = $xml.find('result');
-  console.log($xml.find('result').text());
-  console.log(result.tagName);
 
-  var new_response = $("<div class='response-item'><h4>" + res + "</h4></div>").hide();
+  if ($xml.find('result').text() == '00') {
+    var style = 'success';
+    var icon = 'ok';
+  } else {
+    var style = 'error';
+    var icon = 'remove';
+  }
+
+  const new_resp = $(
+     "<div class='panel-group " + style + "'>"
+      + "<div class='panel panel-default'>"
+        + "<div class='panel-heading'>"
+          + "<h4 class='panel-title'>"
+            + "<span class='glyphicon glyphicon-" + icon + "' aria-hidden='true'></span>"
+            + "<a data-toggle='collapse' href='#" + $xml.find('orderid').text() + "'>"
+              + "Result Code : " + $xml.find('result').text()
+            + "</a>"
+          + "</h4>"
+        + "</div>"
+        + "<div id='" + $xml.find('orderid').text() + "' class='panel-collapse collapse'>"
+          + "<div class='panel-body'>"
+            + "<p>Message : " + $xml.find('message').text() + "</p>"
+            + "<p>CVN Result : " + $xml.find('cvnresult').text() + "</p>"
+            + "<p>Batch ID : " + $xml.find('batchid').text() + "</p>"
+            + "<p>Auth Code : " + $xml.find('batchid').text() + "</p>"
+          + "</div>"
+        + "</div>"
+      + "</div>"
+  ).hide();
 
   // Append the item to the responses div
-  $("#responses").append(new_response);
+  $("#responses").append(new_resp);
 
   // Show the response
-  new_response.show('normal');
+  new_resp.show('normal');
 
   // After 10 seconds delete the object
   setTimeout(function() {
-    new_response.hide('slow', function() { new_response.remove(); });
-  }, 15000);
+    new_resp.hide('slow', function() { new_resp.remove(); });
+  }, 150000);
 }
