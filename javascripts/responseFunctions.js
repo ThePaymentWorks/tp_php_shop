130e140745081fb3c91403724e7d7a74ce372d8e
James Byrne <jamesdesbyrne@gmail.com>
2016-07-06 15:15:26 +0100

    Adding Currency, retry, otherProcessor and highlight functions
    These functions are now fully working

--- javascripts/responseFunctions.js
+++ javascripts/responseFunctions.js
@@ -1,24 +1,49 @@
-function rest () {
-  // Do nothing 
-}
+// Do nothing
+function rest () {}
+
+// Empty the users cart -> Send user to home page
 function reset () {
-  // Empty cart; go to home page;
+  $.ajax({
+    url: "/api/emptycart",
+    type: "POST",
+  }).done(function (res) {
+    window.location.replace('/');
+  }).fail(function (err) {
+    console.log(err);
+  });
 }
-function threeDSecure () {
-  // Waiting on implementation of 3dSecure
-}
+
+// Waiting on implementation of 3dSecure
+function threeDSecure () {}
+
+// Highlight the elements passed to the function
 function highlight (array) {
-  // Go through array and highlight the selected fields 
+  // Go through array and highlight the selected fields
+  array.forEach(function(item) {
+    item.style.borderColor = "red";
+  });
 }
+
 function contactUs () {
   // Probably just call rest? Look into the intercom integration
 }
+
 function currency (currency) {
   // Change the value of the currency field to the selected currency
+  $(".dropdown").find('.btn').html(currency.textContent + ' <span class="caret"></span>');
+  $(".dropdown").find('.btn').val(currency.textContent);
 }
+
+// Dummy message for paying with paypal (another payment processor)
 function otherProcessor () {
-  // Dummy message for paying with paypal (another payment processor)
+  window.location.replace('https://www.paypal.com/ie/webapps/mpp/home');
 }
+
+// Re-submit the form ensuring a success message
 function retry () {
-  // Re-submit the form ensuring a success message
+  $('#total').val(function (index, value) {
+    return value = 0.00;
+  });
+
+  checkout();
 }
