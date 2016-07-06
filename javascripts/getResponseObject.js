f006f179e89febefe285075ad8cc099e33fb43c1
James Byrne <jamesdesbyrne@gmail.com>
2016-07-06 10:13:55 +0100

    Fixing a spelling error

--- javascripts/getResponseObject.js
+++ javascripts/getResponseObject.js
@@ -8,7 +8,7 @@ function getResponseObject (resultCode) {
     'code': '00',
     'responseCode': '00_success',
     'description': 'Successful – the transaction has processed and you may proceed with the sale.',
-    'checkoutMesage': 'Success. Your purchase is complete.',
+    'checkoutmessage': 'Success. Your purchase is complete.',
     'checkoutOptions': {
       'Ok': rest(),
       'Keep Shopping': reset()
@@ -19,7 +19,7 @@ function getResponseObject (resultCode) {
     'code': '100',
     'responseCode': '100_3dsecure',
     'description': 'Successful – 3D secure verification needed.',
-    'checkoutMesage': 'Pending - your bank requests you verify this purchase. Click Verify to open the bank verification page.',
+    'checkoutmessage': 'Pending - your bank requests you verify this purchase. Click Verify to open the bank verification page.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Verify': threeDSecure()
@@ -30,7 +30,7 @@ function getResponseObject (resultCode) {
     'code': '101',
     'responseCode': '101_declined_bank',
     'description': 'A failed transaction specifically meaning: Declined by Bank – generally insufficient funds or incorrect expiry date.',
-    'checkoutMesage': 'We\'re sorry but your bank has declined your transaction. Please confirm your card CVV and expiry date, then try again.',
+    'checkoutmessage': 'We\'re sorry but your bank has declined your transaction. Please confirm your card CVV and expiry date, then try again.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Review Detail': {
@@ -43,7 +43,7 @@ function getResponseObject (resultCode) {
     'code': '102',
     'responseCode': '102_declined_offline',
     'description': 'A failed transaction specifically meaning: Transaction Declined Pending Offline Authorisation..',
-    'checkoutMesage': 'We\'re sorry but your bank has declined your transaction. Contact your bank at +3538787878 to find out why.',
+    'checkoutmessage': 'We\'re sorry but your bank has declined your transaction. Contact your bank at +3538787878 to find out why.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Phone Bank': rest()
@@ -54,7 +54,7 @@ function getResponseObject (resultCode) {
     'code': '103',
     'responseCode': '103_card_stolen',
     'description': 'A failed transaction specifically meaning: Card reported lost or stolen.',
-    'checkoutMesage': 'We\'re sorry but it looks like you\'re using a stolen card. We\'ve suspended your account pending further details.',
+    'checkoutmessage': 'We\'re sorry but it looks like you\'re using a stolen card. We\'ve suspended your account pending further details.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -65,7 +65,7 @@ function getResponseObject (resultCode) {
     'code': '107',
     'responseCode': '107_fraud',
     'description': 'A failed transaction specifically meaning: Your fraud checks blocked the transaction.',
-    'checkoutMesage': 'We\'re sorry, you seem to be a fraudster. Contact us if you wish to dispute this.',
+    'checkoutmessage': 'We\'re sorry, you seem to be a fraudster. Contact us if you wish to dispute this.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -76,7 +76,7 @@ function getResponseObject (resultCode) {
     'code': '600',
     'responseCode': '600_error',
     'description': 'Generic error 600',
-    'checkoutMesage': 'Empty message',
+    'checkoutmessage': 'Empty message',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -87,7 +87,7 @@ function getResponseObject (resultCode) {
     'code': '601',
     'responseCode': '601_error',
     'description': 'Generic error 601',
-    'checkoutMesage': '',
+    'checkoutmessage': '',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -98,7 +98,7 @@ function getResponseObject (resultCode) {
     'code': '603',
     'responseCode': '603_error',
     'description': 'Generic error 603',
-    'checkoutMesage': '',
+    'checkoutmessage': '',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -109,7 +109,7 @@ function getResponseObject (resultCode) {
     'code': '666',
     'responseCode': '666_deactivated',
     'description': 'Account deactivated',
-    'checkoutMesage': '',
+    'checkoutmessage': '',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -120,7 +120,7 @@ function getResponseObject (resultCode) {
     'code': '200',
     'responseCode': '200_bank_comm',
     'description': 'A failed transaction specifically meaning: bank communication error.',
-    'checkoutMesage': '',
+    'checkoutmessage': '',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -131,7 +131,7 @@ function getResponseObject (resultCode) {
     'code': '205',
     'responseCode': '205_bank_comm',
     'description': 'A failed transaction specifically meaning: bank communication error.',
-    'checkoutMesage': '',
+    'checkoutmessage': '',
     'checkoutOptions': {
       'Cancel': rest(),
       'Contact Us': contactUs()
@@ -142,7 +142,7 @@ function getResponseObject (resultCode) {
     'code': '501',
     'responseCode': '501_already_processed',
     'description': 'This transaction has already been processed.',
-    'checkoutMesage': 'We\'re sorry, a technical issue has stopped your purchase from completing. Your card has not been charged. Re-try to complete the purchase or contact us to talk more about this problem.',
+    'checkoutmessage': 'We\'re sorry, a technical issue has stopped your purchase from completing. Your card has not been charged. Re-try to complete the purchase or contact us to talk more about this problem.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Retry': retry(),
@@ -154,7 +154,7 @@ function getResponseObject (resultCode) {
     'code': '507',
     'responseCode': '507_currency',
     'description': 'Currency/card combination not allowed.',
-    'checkoutMesage': 'We\'re sorry, that item is not available to buy in that currency. You can complete your purchase using one of our alternatives.',
+    'checkoutmessage': 'We\'re sorry, that item is not available to buy in that currency. You can complete your purchase using one of our alternatives.',
     'checkoutOptions': {
       'Cancel': rest(),
       'EUR': currency('EUR'),
@@ -168,7 +168,7 @@ function getResponseObject (resultCode) {
     'code': '509',
     'responseCode': '509_expiry_date',
     'description': 'Card expiry date is invalid',
-    'checkoutMesage': 'We\'re sorry, we could not process your card request. Please verify the card expiry date on your card and retry.',
+    'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the card expiry date on your card and retry.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Verify Expiry': {
@@ -181,7 +181,7 @@ function getResponseObject (resultCode) {
     'code': '502',
     'responseCode': '502_invalid_card_name',
     'description': 'Invalid cardholder name',
-    'checkoutMesage': 'We\'re sorry, we could not process your card request. Please verify the cardholder name and retry.',
+    'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the cardholder name and retry.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Verify Name': {
@@ -195,7 +195,7 @@ function getResponseObject (resultCode) {
     'code': '101',
     'responseCode': '101_cvv_not_match',
     'description': 'A failed transaction specifically meaning: CVV Not Matched.',
-    'checkoutMesage': 'We\'re sorry, we could not process your card request. Please verify the card CVV and retry.',
+    'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the card CVV and retry.',
     'checkoutOptions': {
       'Cancel': rest(),
       'Re-enter CVV': {
@@ -208,7 +208,7 @@ function getResponseObject (resultCode) {
     'code': '101',
     'responseCode': '101_cvv_not_checked',
     'description': 'A failed transaction specifically meaning: CVV Not checked due to circumstances.',
-    'checkoutMesage': 'Warning, there was a problem verifying your CVV. Do you wish to complete the purchase without verifying CVV?',
+    'checkoutmessage': 'Warning, there was a problem verifying your CVV. Do you wish to complete the purchase without verifying CVV?',
     'checkoutOptions': {
       'Cancel': rest(),
       'Re-enter CVV': {
@@ -221,7 +221,7 @@ function getResponseObject (resultCode) {
     'code': '101',
     'responseCode': '101_cvv_issuer_no_cert',
     'description': 'A failed transaction specifically meaning: CVV Issuer not certified.',
-    'checkoutMesage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
+    'checkoutmessage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
     'checkoutOptions': {
       'Cancel': reset(),
       'Try with Paypal': otherProcessor()
@@ -232,7 +232,7 @@ function getResponseObject (resultCode) {
     'code': '101',
     'responseCode': '101_cvv_not_processed',
     'description': 'A failed transaction specifically meaning: CVV Not processed.',
-    'checkoutMesage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
+    'checkoutmessage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
     'checkoutOptions': {
       'Cancel': reset(),
       'Try with Paypal': otherProcessor()
@@ -243,7 +243,7 @@ function getResponseObject (resultCode) {
     'code': 'timeout',
     'responseCode': 'timeout',
     'description': 'Timeout.',
-    'checkoutMesage': 'Sorry, we experienced a problem sending your request to your bank and your transaction was not successfull. ',
+    'checkoutmessage': 'Sorry, we experienced a problem sending your request to your bank and your transaction was not successfull. ',
     'checkoutOptions': {
       'Cancel': rest(),
       'Retry': retry()
