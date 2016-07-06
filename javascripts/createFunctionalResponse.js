45131a7b231738a2e96764d95c24c63367e8b31b
James Byrne <jamesdesbyrne@gmail.com>
2016-07-06 11:20:47 +0100

    Hooking up the modal with its params and getting the buttons to be dynamic
    The buttons are generated synamically with links to their associated
    fuctions. Note that all buttons will close the modal.

--- javascripts/createFunctionalResponse.js
+++ javascripts/createFunctionalResponse.js
@@ -2,6 +2,15 @@ function createFunctionalResponse (resultCode) {
   // Get the appropriate object from the list of responses
   var response = getResponseObject(resultCode);
 
+  // Create the buttons to be held in the modal
+  var buttons = '';
+
+  // Loop through all of the checkout Options and set the buttons
+  Object.keys(response.checkoutOptions).forEach(function (key) {
+    console.log(response.checkoutOptions);
+    buttons = buttons.concat(`<button type="button" onClick='${key}' class="btn btn-default dismiss ${key}" data-dismiss="modal">${key}</button>`);
+  });
+
   // Create the html object
   var func_resp = $(
     `<div class="modal" role="dialog">
@@ -9,14 +18,14 @@ function createFunctionalResponse (resultCode) {
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close dismiss" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">${response.responseCode}</h4>
+            <h4 class="modal-title">${response.description}</h4>
           </div>
           <div class="modal-body">
-            <p>One fine body&hellip;</p>
+            <p>${response.responseCode}</p>
+            <p>${response.checkoutmessage}</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default dismiss" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            ${buttons}
           </div>
         </div>
       </div>
@@ -24,16 +33,16 @@ function createFunctionalResponse (resultCode) {
   ).appendTo('body');
 
   // If the modal already exists replace it, else append modal to the body
-  if ($('body').children(".modal").length == 0) {
+  if ($('body').children(".modal").length === 0) {
     $('body').append(func_resp);
   } else {
     $('body').children(".modal").replaceWith(func_resp);
   }
 
   // Show the response
-  func_resp.hide().fadeIn(1000);
+  func_resp.hide().fadeIn(500);
 
   $('.dismiss').click(function(e){
-    func_resp.hide()
+    func_resp.hide();
   });
 }
