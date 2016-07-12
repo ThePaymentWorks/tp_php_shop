9a8476e35447f0b7b038863f733c01486d49cadb
James Byrne <jamesdesbyrne@gmail.com>
2016-07-12 11:11:14 +0100

    Adding a padding function and organising functions with a setup file

---
+++ javascripts/setup.js
@@ -0,0 +1,18 @@
+// General functions need during setup
+
+$(function () {
+
+  // Pad out the total with 2 decimal places
+  $('#total').val(function () {
+    return parseFloat(this.value).toFixed(2);
+  });;
+
+  // When the total is changed update the convertedTotal
+  $('#total').change(function () {
+    this.value = parseFloat(this.value).toFixed(2);
+    convertedTotal();
+  });
+
+  // Call converted total on page load
+  convertedTotal();
+})
