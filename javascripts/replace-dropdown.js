1bf097550e6f0ceab5a97b9476486ef54ddf7da8
James Byrne <jamesdesbyrne@gmail.com>
2016-10-14 08:21:37 +0100

    Removing all client assets, code and dependencies from the shop

--- javascripts/replace-dropdown.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  // When the user selects another currency change the dropdown button and update the currency
-  $(".dropdown-menu li a").click(function(){
-    $(this).parents(".dropdown").find('.dropdown-id').text($(this).text());
-    // Call converted total when the dropdown is changed
-    convertedTotal();
-  });
-});
